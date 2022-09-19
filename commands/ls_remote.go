b59b23941525a82a2dbbf7d36b8d4acb7a989b36
kmtym1998 <kmtym.devloper@gmail.com>
2022-09-19 22:14:41 +0900

    refactor: remove unused codes

--- commands/ls_remote.go
+++ commands/ls_remote.go
@@ -5,7 +5,6 @@ import (
 	"github.com/kmtym1998/hasuraenv/internal/model"
 	"github.com/kmtym1998/hasuraenv/internal/services"
 	"github.com/samber/lo"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -22,19 +21,12 @@ func NewLsRemoteCmd(ec *cli.ExecutionContext) *cobra.Command {
 			return ec.Prepare()
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			logger := logrus.New()
-			logger.SetOutput(ec.Stdout)
-			logger.SetFormatter(&logrus.TextFormatter{DisableTimestamp: true, DisableColors: ec.NoColor})
-			if !ec.IsTerminal {
-				logger.SetFormatter(&logrus.JSONFormatter{PrettyPrint: false})
-			}
-
 			releases, err := services.ListHasuraReleases()
 			if err != nil {
 				return err
 			}
 
-			logger.InfoFn(func() []interface{} {
+			ec.Logger.InfoFn(func() []interface{} {
 				// FIXME: 全部のリリース取得する できたら limit オブション渡せるようにする
 				messages := []interface{}{"Latest 30 releases"}
 				versions := lo.Map(releases, func(r model.GitHubRelease, _ int) interface{} {
