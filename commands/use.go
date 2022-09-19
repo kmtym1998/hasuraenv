25bde03084bb0056445c48ee5ff5a3ea2ff12d16
kmtym1998 <kmtym.devloper@gmail.com>
2022-09-19 22:33:41 +0900

    chore: update comment

--- commands/use.go
+++ commands/use.go
@@ -24,6 +24,7 @@ func NewUseCmd(ec *cli.ExecutionContext) *cobra.Command {
 
 			// TODO: ローカルに存在するバージョンかどうか検証
 			// TODO: ローカルに存在しないバージョンだったらインストールするかどうか聞く
+			// NOTE: https://dev.to/divrhino/building-an-interactive-cli-app-with-go-cobra-promptui-346n
 
 			return services.ValidateSemVer(args[0])
 		},
