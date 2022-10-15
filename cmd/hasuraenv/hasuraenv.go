2a7ccfc255ca162dc2ce1bb5b57f461d5d23267c
kmtym1998 <kmtym.devloper@gmail.com>
2022-10-15 23:25:01 +0900

    chore: build options

--- cmd/hasuraenv/hasuraenv.go
+++ cmd/hasuraenv/hasuraenv.go
@@ -2,13 +2,34 @@
 package main
 
 import (
+	"os"
+
+	cli "github.com/kmtym1998/hasuraenv"
 	"github.com/kmtym1998/hasuraenv/commands"
 	log "github.com/sirupsen/logrus"
 )
 
+var (
+	version        string = "v0.0.2"
+	configPathBase string = os.Getenv("HOME") + "/.hasuraenv"
+)
+
 // main is the entrypoint function
 func main() {
-	if err := commands.Execute(); err != nil {
+	bo := cli.NewBuildOptions(version, configPathBase)
+	ec := cli.NewExecutionContext(bo)
+	rootCmd := commands.NewRootCmd()
+
+	rootCmd.AddCommand(
+		commands.NewVersionCmd(ec),
+		commands.NewInitCmd(ec),
+		commands.NewLsRemoteCmd(ec),
+		commands.NewLsCmd(ec),
+		commands.NewInstallCmd(ec),
+		commands.NewUseCmd(ec),
+	)
+
+	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
